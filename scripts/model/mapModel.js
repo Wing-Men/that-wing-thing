1ba1470835f7d9bddec8b4fd4c147463bb1704ce
Waxhoya <waxhoya@gmail.com>
2016-10-10 14:43:40 -0700

    IFFE refactoring of the mapModel.js

--- scripts/model/mapModel.js
+++ scripts/model/mapModel.js
@@ -1,13 +1,16 @@
 'use strict';
+(function(module){
+  function initMap() {
+    var portland = {lat: 45.5163719, lng: -122.6765228};
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: portland  });
 
-function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-}
+    var marker = new google.maps.Marker({
+      position: portland,
+      map: map
+    });
+  }
+  window.initMap = initMap;
+})(window);
