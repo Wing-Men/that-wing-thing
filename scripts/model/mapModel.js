f99114d83af67a4e50f8c01d6f5281e6a8f70054
Waxhoya <waxhoya@gmail.com>
2016-10-10 16:41:56 -0700

    map marker build out for night

--- scripts/model/mapModel.js
+++ scripts/model/mapModel.js
@@ -1,16 +1,27 @@
 'use strict';
 (function(module){
+  var portland = {lat: 45.5163719, lng: -122.6765228};
+
+  var AlamedaBrewCo = {lat: 45.548459, lng: -122.613639};
+  var AlbertStreetPub = {lat: 45.5589999, lng: -122.6569361};
+
+  var mapOptions = {
+    center: portland,
+    zoom: 10,
+  }
+
   function initMap() {
-    var portland = {lat: 45.5163719, lng: -122.6765228};
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: portland  });
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
     var marker = new google.maps.Marker({
-      position: portland,
+      position: AlamedaBrewCo,
       map: map
     });
+    new google.maps.Marker({
+      position: AlbertStreetPub,
+      map: map
+    });
+
   }
   window.initMap = initMap;
 })(window);
