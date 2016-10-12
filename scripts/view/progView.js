d69fe1ecc64a99584620d0e31e40d4dfc5e11c02
willnickerson <wnickers@willamette.edu>
2016-10-12 11:12:29 -0700

    bar chart animations tightened and chart hooked up

--- scripts/view/progView.js
+++ scripts/view/progView.js
@@ -5,9 +5,24 @@
   UserProgressView.renderProgress = function() {
     //grab the fav total visited and list of visited from currUser
     //render some sort of chart
+    var markerCount = 0;
     var progress = (currUser.totalVisited * 100 / 19).toString();
     console.log(progress);
     $('#wing-walk').animate({width: progress + '%'},1500, 'swing');
+    if(progress >= 25 && progress <= 49) {
+      markerCount = 1;
+    } else if(progress >= 50 && progress <= 74) {
+      markerCount = 2;
+    } else if(progress >= 75 && progress <= 99) {
+      markerCount = 3;
+    } else {
+      markerCount = 4;
+    }
+    var timeInterval = 1500/markerCount
+    for(var i = 1; i <= markerCount; i++) {
+      var $marker = $('#marker-filled-' + i.toString());
+      $marker.fadeIn(timeInterval * i);
+    }
   };
 
   UserProgressView.updateUserProgress = function(){
